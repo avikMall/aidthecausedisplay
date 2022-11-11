ce60721e9b7784b1e9d4b4fe869f228c9a8391a3
avikMall <69607237+avikMall@users.noreply.github.com>
2022-11-11 15:56:20 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) config.resolve.fallback.fs = false;
+    if (!isServer) config.resolve.fallback.child_process = false;
+    if (!isServer) config.resolve.fallback.net = false;
+    if (!isServer) config.resolve.fallback.http2 = false;
+    if (!isServer) config.resolve.fallback.tls = false;
+    return config;
+  }
+};
